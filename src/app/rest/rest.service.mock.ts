3c87cacbf43730959114a76241943fc6d4792c17
ntsokos <ntsokos@gmail.com>
2017-10-12 22:33:38 +0100

    Added data provided by edinburgh council mock data retrieved through origin (and not using a harcoded domain)

--- src/app/rest/rest.service.mock.ts
+++ src/app/rest/rest.service.mock.ts
@@ -9,17 +9,19 @@ import { Md5 } from 'ts-md5/dist/md5';
 import { BusTimesRequest } from './records/bus-times-request';
 import { BusTimesResponse } from './records/bus-times-response';
 import { TimeRequest } from './records/time-request';
+import { PlatformLocation } from '@angular/common';
 
 @Injectable()
 export class RestServiceMock {
 
-  constructor(private http: HttpClient, private settings: SettingsContext) { }
+  constructor(private http: HttpClient, private platformLocation: PlatformLocation) { }
 
   public getBusTimesforStopId(stopId: string): Observable<BusTimesResponse> {
-
-    const url = 'http://localhost:4200/assets/getbustimes-response.json';
+    const origin = (this.platformLocation as any).location.origin;
+    const url = `${origin}/assets/getbustimes-response.json`;
 
     return this.http.get(url).map(response => {
+
       const busTimesResponse = new BusTimesResponse();
       Object.assign(busTimesResponse, response);
       return busTimesResponse;
